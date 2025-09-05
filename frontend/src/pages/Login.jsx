1814f3fb78fdad8e404f0b0460a61e5128f4673e
Munyankindi François <munyankindif0@gmail.com>
2025-09-05 11:53:51 +0200

    login changes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
 
 export default function Login() {
   return (
-    <div>Login</div>
-  )
+    <section>
+      hello world!{" "}
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure doloremque
+        autem culpa. Ea ipsam numquam voluptatum vitae perspiciatis! Illo iusto
+        modi adipisci sit dolorem? Sequi vitae voluptatem rerum atque
+        architecto?
+      </p>
+    </section>
+  );
 }
