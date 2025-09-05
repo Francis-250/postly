fa3f3ce92fb11c4da95b16ce55f944178637992a
vicious-franco <irakaramale@gmail.com>
2025-09-05 11:38:44 +0200

    changes on nav

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div>
+      <nav className="bg-gray-800 p-4 text-white">
+        <h1 className="text-xl font-bold">My Navbar</h1>
+      </nav>
+    </div>
+  );
 }
